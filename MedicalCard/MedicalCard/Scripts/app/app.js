c89c41d81be2e50282a6cb2d9a6c46b414ada9e8
Mikhail Kuznetsov <Mikhail_Kuznetsov@EPRUSARW0785.saratov.epam.com>
2015-02-27 13:52:03 +0300

    Base architecture

---
+++ MedicalCard/MedicalCard/Scripts/app/app.js
@@ -0,0 +1,12 @@
+﻿(function () {
+
+	'use strict';
+
+	angular.module('medicalCardApp', [
+		'ngRoute',
+		'toastr',
+		'ngAnimate',
+		'ngResource'
+	]);
+
+}());
---
+++ MedicalCard/MedicalCard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MedicalCard.Startup))]
+namespace MedicalCard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
