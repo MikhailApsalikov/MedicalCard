9e8f19844714c2f4054142b0c4c9a994ecfa6050
Mikhail Kuznetsov <Mikhail_Kuznetsov@EPRUSARW0785.saratov.epam.com>
2015-03-16 14:22:16 +0300

    NavBar directive and registration

--- MedicalCard/MedicalCard/Scripts/app/controllers/account/register.js
+++ MedicalCard/MedicalCard/Scripts/app/controllers/account/register.js
@@ -3,7 +3,7 @@
 
 	angular.module('medicalCardApp')
 
-	.controller('registerController', ['$scope', 'Accounts', 'Enums', function ($scope, Accounts, Enums) {
+	.controller('registerController', ['$scope', 'Accounts', 'Enums', 'Doctors', function ($scope, Accounts, Enums, Doctors) {
 
 		$scope.account = new Accounts();
 
@@ -12,12 +12,31 @@
 		$scope.register = function () {
 
 			$scope.account.$save(function (response) {
-				console.log(response);
 				$scope.registerFirstStep = false;
+				console.log(response);
+				if (response.Role === Enums.roleCodes.doctor) {
+					$scope.doctorCreationMode = true;
+					$scope.doctor = new Doctors();
+					$scope.doctor.id = response.Id;
+					$scope.doctor.account = {
+						username: response.Username,
+						password: response.Password,
+						id: response.Id,
+						role: response.Role
+					}
+				}
 			});
 
-
 		};
+
+		$scope.saveDoctor = function () {
+
+			$scope.doctor.$save(function () {
+
+				alert('ok');
+			});
+
+		}
 
 	}])
 
---
+++ MedicalCard/MedicalCard/Scripts/app/directives/setActiveTab.js
@@ -0,0 +1,16 @@
+﻿(function () {
+	'use strict';
+
+	angular.module('medicalCardApp')
+		.directive('setActiveTab', function () {
+			return {
+				restrict: 'A',
+				link: function (scope, element, attrs) {
+					scope.$on('$routeChangeSuccess', function (event, next, current) {
+						element.find('.active').toggleClass('active', false);
+						element.find('a[href="#' + next.originalPath + '"]').closest('li').toggleClass('active').closest('.dropdown').toggleClass('active');
+					});
+				}
+			}
+		});
+}());
