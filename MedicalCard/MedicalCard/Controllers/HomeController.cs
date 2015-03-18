04ef438e70df8df62e7a9c2eac701043869ab307
MikhailApsalikov <Mikhail_Apsalikov@epam.com>
2015-03-18 14:53:00 +0300

    Login modified

--- MedicalCard/MedicalCard/Controllers/HomeController.cs
+++ MedicalCard/MedicalCard/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿namespace MedicalCard.Controllers
 {
 	using System.Linq;
+	using System.Net;
+	using System.Web;
 	using System.Web.Mvc;
 	using BLL;
 	using BLL.Repositories;
@@ -20,25 +22,24 @@
 				repository.GetAll().FirstOrDefault(acc => acc.Username == username);
 			if (account == null)
 			{
+				Response.StatusCode = (int)HttpStatusCode.NotFound;
 				return Json(new
 				{
-					success = false,
 					reason = "Пользователь не найден"
 				});
 			}
 
 			if (account.Password != password)
 			{
+				Response.StatusCode = (int)HttpStatusCode.NotFound;
 				return Json(new
 				{
-					success = false,
 					reason = "Неверный пароль"
 				});
 			}
 
 			return Json(new
 			{
-				success = true,
 				id = account.Id,
 				username = account.Username,
 				role = account.Role,
